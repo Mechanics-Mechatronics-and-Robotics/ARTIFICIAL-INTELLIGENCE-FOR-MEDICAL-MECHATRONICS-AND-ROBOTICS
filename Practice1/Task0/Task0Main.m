1d2f3c5b6e31cc68ced89c4d418c04c5dd796b06
Alexey Kornaev <70199936+avkornaev@users.noreply.github.com>
2021-02-07 12:02:49 +0300

    Add files via upload

---
+++ Practice1/Task0/Task0Main.m
@@ -0,0 +1,27 @@
+%% Task 0: Warming-up
+clear ; close all; clc
+
+%% 1. Create a scalar (matrix, structure)
+ %Create a scalar
+
+ %Create 2 matrices
+
+  %return an element
+
+  %return a column
+
+  %return a raw
+
+  %transpose a matrix
+
+  %product 2 matrices
+
+  %element-wise product of 2 matrices
+
+  %merge 2 matrices
+
+ %Create a structure
+
+  %return an element
+  
+%% 2. Create a function
